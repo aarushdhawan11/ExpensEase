3a2706982f55b869339b1a47a95757fcf8b8937e
Aarush Dhawan <122430054+aarushdhawan1511@users.noreply.github.com>
2025-04-21 11:02:58 +0530

    added

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const db = async () =>{
     try{
         mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect("mongodb+srv://aakasshhh:^vvrulc2q@noteapp.pxzl6.mongodb.net/?retryWrites=true&w=majority&appName=Noteapp")
         console.log("Database Connected");
     } catch(error){
         console.log("Database Connection Error");
